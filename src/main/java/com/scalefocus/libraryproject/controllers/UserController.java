ec0ae2a9535e5a06456a95912a5e86c57da93fbf
Stofko <114482254+fycevo@users.noreply.github.com>
2024-11-21 15:13:30 +0200

    Added a soft delete method and created a sql script to handle the status of each user

--- src/main/java/com/scalefocus/libraryproject/controllers/UserController.java
+++ src/main/java/com/scalefocus/libraryproject/controllers/UserController.java
@@ -3,6 +3,9 @@ package com.scalefocus.libraryproject.controllers;
 import com.scalefocus.libraryproject.models.UserModel;
 import com.scalefocus.libraryproject.services.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
@@ -16,7 +19,15 @@ public class UserController {
     }
     @GetMapping("/users/{id}")
     public UserModel getUser(@PathVariable Long id){
-
         return userService.getUser(id);
+    }
+    @DeleteMapping("/users/{id}")
+    public ResponseEntity<String> deleteUser(@PathVariable Long id) {
+        boolean isDeleted = userService.deleteUser(id);
+        if(isDeleted) {
+            return ResponseEntity.status(HttpStatus.CREATED).body("User deleted successfully");
+        } else {
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).body("User not found");
+        }
     }
 }
